dfb16d5d16f6b7b82e07f74c2c76f59a04216529
urbanrustic <ankershtok77@gmail.com>
2021-09-22 19:32:49 +0300

    Don't end version, change method of array

--- Home1.c
+++ Home1.c
@@ -1,7 +1,46 @@
 #include <stdio.h>
+#include <stdbool.h>
 
+// int power(int a, int b) {
+// 	int c = 1;
+// 	while (b) {
+// 		c*= a;
+// 		b--;
+// 	}
+// 	return c;
+// }
+
+//Функция для отсева, если число не состоит только из двоичного кода программа закрывается
+int isbool(char s) {
+	if ((s != '1') && (s != '0')) return 0;
+	else return 1;
+}
 
 int main() {
-	printf("Hello world!");
+	char sym, array_num[32] = {};
+	int i = 0, count = 0;
+
+	while ((sym = getchar()) != '\n') {
+		if (isbool(sym)) {
+			if (i > 32) return 1;
+			array_num[i++] = sym;
+		}
+		else return 1;
+	}
+	
+	//Идёт с начала массива, надо чтоб шла с конца
+	if (array_num[31] == '\0') {
+		for (int k = 0; k < i; k++) {
+			count = (array_num[k]-'0') + count*2;
+		}
+	}
+	else {
+		for (int k = 0; k < i-1; k++) {
+			count = (array_num[k]-'0') + count*2;
+		}
+		count = ~(count-1);
+	}
+	
+	printf("%d\n", count);
 	return 0;
 }
