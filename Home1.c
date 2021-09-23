c9ba349fb54c89e723c3f6d23861af5d3f1024ee
urbanrustic <ankershtok77@gmail.com>
2021-09-23 16:21:46 +0300

    End version of Homework file

--- Home1.c
+++ Home1.c
@@ -3,47 +3,43 @@
 
 //Функция для отсева, если число не состоит только из двоичного кода программа закрывается
 int isbool(char s) {
-	if ((s != '1') && (s != '0')) {
-		printf("You made a mistake.");
-		return 0;
-	}
-	else return 1;
+    if ((s != '1') && (s != '0')) {
+        printf("You made a mistake. There must be letters and numbers from 2 to 9.\n");
+        return 0;
+    } else return 1;
 }
 
-//Функция для перевода в десятичное число из двоичного
-int counter(int min, int max, char* arr) {
-	int k = 0;
-	for (int i = min; i < max; i++)
-	{
-		k = (arr[i]-'0') + k*2;
-	}
-	return k;
+//Функция для перевода двоичного числа в десятичное 
+int bin_dec(int min, int max, char * arr) {
+    int k = 0;
+    for (int i = min; i < max; i++) {
+        k = (arr[i] - '0') + k * 2;
+    }
+    return k;
 }
-
 
 int main() {
-	char sym, array_num[32] = {};
-	int i = 0, count = 0;
+    char sym, array_bin[32] = {};
+    int counter = 0, res = 0;
 
-	while ((sym = getchar()) != '\n') {
-		if (isbool(sym)) {
-			if (i > 32) {
-				printf("You entered too large a number");
-				return 1;
-			}
-			array_num[i++] = sym;
-		}
-		else return 1;
-	}
-	
-	if ((array_num[0]-'0') == 1 && (array_num[31] != '\0')) {
-		count = counter(1, 32, array_num);
-		count = ~(2147483647-count);	
-	}
-	else {
-		count = counter(0, i, array_num);
-	}
-	
-	printf("%d\n", count);
-	return 0;
+    while ((sym = getchar()) != '\n') {
+        if (isbool(sym)) {
+            // Проверка на переполнение
+            if (counter > 32) {
+                printf("You entered too large a number!\n");
+                return 1;
+            }
+            array_bin[counter++] = sym;
+        } else return 1;
+    }
+
+    if ((array_bin[0] - '0') == 1 && (array_bin[31] != '\0')) {
+        res = bin_dec(1, 32, array_bin);
+        res = ~(2147483647 - res);
+    } else {
+        res = bin_dec(0, counter, array_bin);
+    }
+
+    printf("%d\n", res);
+    return 0;
 }
