966750f9b87e2930c410ff1aedf33f258a0c717f
urbanrustic <ankershtok77@gmail.com>
2021-09-23 09:52:09 +0300

    Ending of version

--- Home1.c
+++ Home1.c
@@ -1,20 +1,25 @@
 #include <stdio.h>
-#include <stdbool.h>
 
-// int power(int a, int b) {
-// 	int c = 1;
-// 	while (b) {
-// 		c*= a;
-// 		b--;
-// 	}
-// 	return c;
-// }
 
 //Функция для отсева, если число не состоит только из двоичного кода программа закрывается
 int isbool(char s) {
-	if ((s != '1') && (s != '0')) return 0;
+	if ((s != '1') && (s != '0')) {
+		printf("You made a mistake.");
+		return 0;
+	}
 	else return 1;
 }
+
+//Функция для перевода в десятичное число из двоичного
+int counter(int min, int max, char* arr) {
+	int k = 0;
+	for (int i = min; i < max; i++)
+	{
+		k = (arr[i]-'0') + k*2;
+	}
+	return k;
+}
+
 
 int main() {
 	char sym, array_num[32] = {};
@@ -22,23 +27,21 @@ int main() {
 
 	while ((sym = getchar()) != '\n') {
 		if (isbool(sym)) {
-			if (i > 32) return 1;
+			if (i > 32) {
+				printf("You entered too large a number");
+				return 1;
+			}
 			array_num[i++] = sym;
 		}
 		else return 1;
 	}
 	
-	//Идёт с начала массива, надо чтоб шла с конца
-	if (array_num[31] == '\0') {
-		for (int k = 0; k < i; k++) {
-			count = (array_num[k]-'0') + count*2;
-		}
+	if ((array_num[0]-'0') == 1 && (array_num[31] != '\0')) {
+		count = counter(1, 32, array_num);
+		count = ~(2147483647-count);	
 	}
 	else {
-		for (int k = 0; k < i-1; k++) {
-			count = (array_num[k]-'0') + count*2;
-		}
-		count = ~(count-1);
+		count = counter(0, i, array_num);
 	}
 	
 	printf("%d\n", count);
